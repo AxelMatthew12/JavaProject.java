1250ee5e7f12f9f5ef9a024e26039d40e609db9a
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2023-12-08 08:25:57 +0700

    BetaFiturlengkap

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main{
+    
+    static void login(){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("======SELAMAT DATANG======");
+        System.out.println("        KANTIN SEHAT      ");
+        System.out.println("       LOGIN SEBAGAI      ");
+        System.out.println("                          ");
+        System.out.println("          MANAJER         ");
+        System.out.println("          KASIER          ");
+        System.out.println("==========================");
+        int pilih= scanner.nextInt();
+        }
+
+        static String loginmanajer(String pilihan){
+            System.out.println("MASUKKAN NAMEID:   ");
+            System.out.println("MASUKKAN PASSCODE: ");
+
+
+        }
+
+
+
+}
